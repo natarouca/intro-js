69b2f6b2cdc5ca8fed47787f8eef0dc9d4749256
nathallya santos <nathallya373@gmail.com>
2025-09-03 22:24:24 -0300

    Condicional if-else simples

---
+++ js/04-if-else.js
@@ -0,0 +1,19 @@
+// Estrutura base do laço condicional if-else
+/*
+
+    No bloco if, acontece a comparação de valores, onde a condição pode ou não ser atendida.
+        if (condição) {
+            Execute este bloco de código se a condição for verdadeira.
+         } else {
+             Caso contrário, execute este bloco de código.
+         }
+
+*/
+
+// Exemplo de condicional simples
+
+let idade = 18;
+if (idade >= 18) {
+    console.log("Você é maior de idade.");
+} 
+// Nesse caso, não há uma instrução para casos onde a condição não é atendida.
