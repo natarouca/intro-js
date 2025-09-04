f1d97e44c8604cfe8ba9ad4b30338572163441bf
nathallya santos <nathallya373@gmail.com>
2025-09-03 22:53:58 -0300

    Condicional composta

---
+++ js/05-if-else.js
@@ -0,0 +1,19 @@
+
+// Condicional composta (if e else)
+    // Na condicional composta, há uma estrutura complementar, ou sej, um bloco de código com instruções para lidar com a negação do valor lógico de if (falso).
+    
+    let idade = 18;
+    if (idade >= 18) {
+        
+        // Se a variável idade atender a condição estabelecida, este bloco de código é executado.
+        console.log("Você é maior de idade.");
+        alert("Idade permitida!")
+        // Valor lógico: true
+    } else {
+        
+        // Caso a condição não for atendida, este bloco de código é executado.
+        console.log("Você é menor de idade");
+        alert("Idade não permitida!");
+        // Valor lógico: false. 
+    }
+    
